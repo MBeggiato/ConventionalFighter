d4552c44b23d6e101fad1e5410991d95ff52d016
Marcel <marcelbeggiato@pm.me>
2024-11-06 20:25:06 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let counter = $state(0);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>{counter}</p>
+<button onclick={() => counter++}>+</button>
+<button onclick={() => counter--}>-</button>
